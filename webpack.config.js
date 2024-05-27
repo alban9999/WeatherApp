590d5d5c9921914df26cd1281fa468f7dcdb2214
Alban Abduli <alban.abduli@outlook.com>
2024-05-27 16:17:09 +0200

    Added the webpack mode: development!

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './index.web.js', // Entry point for web app
   module: {
     rules: [
